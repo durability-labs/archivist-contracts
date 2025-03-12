700552c2bce6e9fe0154d5df3e3e4e2bf5bf0e34
Mark Spanbroek <mark@spanbroek.net>
2025-03-12 10:52:11 +0100

    marketplace: repair reward is paid out at the end
    It is no longer a discount on the collateral, to
    simplify reasoning about collateral in the sales
    module of the codex node

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -348,33 +348,18 @@ describe("Marketplace", function () {
         await marketplace.freeSlot(slotId(slot))
       })
 
-      it("gives the host a discount on the collateral", async function () {
-        const collateral = collateralPerSlot(request)
-        const reward = repairReward(config, collateral)
-        const discountedCollateral = collateral - reward
-        const marketplaceAddress = await marketplace.getAddress()
-        await token.approve(marketplaceAddress, discountedCollateral)
-        await marketplace.reserveSlot(slot.request, slot.index)
-        const startBalance = await token.balanceOf(host.address)
-        await marketplace.fillSlot(slot.request, slot.index, proof)
-        const endBalance = await token.balanceOf(host.address)
-
-        expect(startBalance - endBalance).to.equal(discountedCollateral)
-      })
-
       it("tops up the host collateral with the repair reward", async function () {
         const collateral = collateralPerSlot(request)
         const reward = repairReward(config, collateral)
-        const discountedCollateral = collateral - reward
         const marketplaceAddress = await marketplace.getAddress()
-        await token.approve(marketplaceAddress, discountedCollateral)
+        await token.approve(marketplaceAddress, collateral)
         await marketplace.reserveSlot(slot.request, slot.index)
 
         const startBalance = await marketplace.getSlotBalance(slotId(slot))
         await marketplace.fillSlot(slot.request, slot.index, proof)
         const endBalance = await marketplace.getSlotBalance(slotId(slot))
 
-        expect(endBalance - startBalance).to.equal(collateral)
+        expect(endBalance - startBalance).to.equal(collateral + reward)
       })
     })
 
