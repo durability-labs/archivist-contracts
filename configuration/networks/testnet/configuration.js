a52fce225a73fd7ae6f9d974361339f2dd6a7a26
Slava <20563034+veaceslavdoina@users.noreply.github.com>
2025-10-09 20:04:23 +0300

    feat: testnet configuration via env-vars with arbitrum adjusted defaults

---
+++ configuration/networks/testnet/configuration.js
@@ -0,0 +1,23 @@
+function asNumber(value) {
+  return parseInt(value);
+}
+
+module.exports = {
+  collateral: {
+    repairRewardPercentage: asNumber(process.env.TESTNET_REPAIRREWARD ? process.env.TESTNET_REPAIRREWARD : 10),
+    maxNumberOfSlashes: asNumber(process.env.TESTNET_MAXSLASHES ? process.env.TESTNET_MAXSLASHES : 2),
+    slashPercentage: asNumber(process.env.TESTNET_SLASHPERCENTAGE ? process.env.TESTNET_SLASHPERCENTAGE : 20),
+    validatorRewardPercentage: asNumber(process.env.TESTNET_VALIDATORREWARD ? process.env.TESTNET_VALIDATORREWARD : 20),
+  },
+  proofs: {
+    period: asNumber(process.env.TESTNET_PERIOD ? process.env.TESTNET_PERIOD : 20),
+    timeout: asNumber(process.env.TESTNET_TIMEOUT ? process.env.TESTNET_TIMEOUT : 10),
+    downtime: asNumber(process.env.TESTNET_DOWNTIME ? process.env.TESTNET_DOWNTIME : 128),
+    downtimeProduct: asNumber(process.env.TESTNET_DOWNTIMEPRODUCT ? process.env.TESTNET_DOWNTIMEPRODUCT : 131),
+    zkeyHash: "",
+  },
+  reservations: {
+    maxReservations: asNumber(process.env.TESTNET_MAXRESERVATIONS ? process.env.TESTNET_MAXRESERVATIONS : 3),
+  },
+  requestDurationLimit: asNumber(process.env.TESTNET_MAXDURATION ? process.env.TESTNET_MAXDURATION : 60*60*24*30) // 30 days
+}
