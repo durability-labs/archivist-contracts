codex-storage/codex-contracts-eth

marketplace: burn tokens in vault when slashing
- move all collateral calculatons to separate library

contracts/TestMarketplace.sol
@@ -5,6 +5,9 @@ import "./Marketplace.sol";
 
 // exposes internal functions of Marketplace for testing
 contract TestMarketplace is Marketplace {
+  using VaultHelpers for RequestId;
+  using VaultHelpers for Vault;
+
   constructor(
     MarketplaceConfig memory config,
     Vault vault,
@@ -15,8 +18,11 @@ contract TestMarketplace is Marketplace {
     _forciblyFreeSlot(slotId);
   }
 
-  function getSlotCollateral(SlotId slotId) public view returns (uint256) {
-    return _slots[slotId].currentCollateral;
+  function getSlotBalance(SlotId slotId) public view returns (uint256) {
+    Slot storage slot = _slots[slotId];
+    Fund fund = slot.requestId.asFund();
+    AccountId account = vault().hostAccount(slot.host, slot.slotIndex);
+    return vault().getBalance(fund, account);
   }
 
   function challengeToFieldElement(

durability-labs/archivist-contracts

contracts/TestMarketplace.sol
@@ -5,6 +5,9 @@ import "./Marketplace.sol";
 
 // exposes internal functions of Marketplace for testing
 contract TestMarketplace is Marketplace {
+  using VaultHelpers for RequestId;
+  using VaultHelpers for Vault;
+
   constructor(
     MarketplaceConfig memory config,
     Vault vault,
@@ -15,8 +18,11 @@ contract TestMarketplace is Marketplace {
     _forciblyFreeSlot(slotId);
   }
 
-  function getSlotCollateral(SlotId slotId) public view returns (uint256) {
-    return _slots[slotId].currentCollateral;
+  function getSlotBalance(SlotId slotId) public view returns (uint256) {
+    Slot storage slot = _slots[slotId];
+    FundId fund = slot.requestId.asFundId();
+    AccountId account = vault().hostAccount(slot.host, slot.slotIndex);
+    return vault().getBalance(fund, account);
   }
 
   function challengeToFieldElement(